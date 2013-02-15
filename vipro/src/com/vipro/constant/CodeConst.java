06c1c8b31bc93225113dac23ab644e457e014882
joyzhin@gmail.com <joyzhin@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-02-15 10:16:35 +0000

    Add common code for Country, State, City and Status

--- vipro/src/com/vipro/constant/CodeConst.java
+++ vipro/src/com/vipro/constant/CodeConst.java
@@ -6,4 +6,12 @@ public interface CodeConst {
 	public static final String DEPARTMENT = "DEPARTMENT";
 	
 	public static final String CONTACT_TITLE = "CONTACT_TITLE";
+	
+	public static final String COUNTRY = "COUNTRY";
+	
+	public static final String STATE = "STATE";
+	
+	public static final String CITY = "CITY";
+	
+	public static final String STATUS = "STATUS";
 }
