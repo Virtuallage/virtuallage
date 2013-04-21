d4e8451265b478733877ba59b116ab0b19dda9a4
joyzhin@gmail.com <joyzhin@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-04-21 09:21:48 +0000

    Integrate reports (sales_reg_form, sales_reg_receipt) with application

---
+++ trunk/vipro/src/com/vipro/constant/JasperConst.java
@@ -0,0 +1,17 @@
+package com.vipro.constant;
+
+
+public class JasperConst {
+
+	
+	public static final String TOMCAT_HOME = System.getProperty("catalina.base") + "/wtpwebapps";
+	
+	public static final String SALES_REG_RECEIPT = TOMCAT_HOME + "/vipro/report/sales_reg_receipt.jrxml";
+
+	public static final String SALES_REG_FORM = TOMCAT_HOME + "/vipro/report/sales_reg_form.jrxml";
+
+	public static final String TMP_PATH = System.getProperty("java.io.tmpdir") + "/";
+	
+	public static final String TMP_FILENAME = "jasper_tmp";
+
+}
