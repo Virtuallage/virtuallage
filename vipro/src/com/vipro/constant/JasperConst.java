a6e7b93bc0b56db618fca12b9904f195bbb759a2
joyzhin@gmail.com <joyzhin@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-04-28 10:04:01 +0000

    Add MIS report for Project Pricing

--- vipro/src/com/vipro/constant/JasperConst.java
+++ vipro/src/com/vipro/constant/JasperConst.java
@@ -9,6 +9,8 @@ public class JasperConst {
 	public static final String SALES_REG_RECEIPT = TOMCAT_HOME + "/vipro/report/sales_reg_receipt.jrxml";
 
 	public static final String SALES_REG_FORM = TOMCAT_HOME + "/vipro/report/sales_reg_form.jrxml";
+	
+	public static final String MIS_PROJECT_PRICING = TOMCAT_HOME + "/vipro/report/mis_proj_pricing.jrxml";
 
 	public static final String TMP_PATH = System.getProperty("java.io.tmpdir") + "/";
 	
