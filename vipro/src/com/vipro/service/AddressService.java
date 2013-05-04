6f88f0a294a2fff5d3cf94e1c3348776fa8b841d
lancelot.mok@gmail.com <lancelot.mok@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-05-04 12:56:06 +0000

    Check in by Lance: Customer Service module

--- vipro/src/com/vipro/service/AddressService.java
+++ vipro/src/com/vipro/service/AddressService.java
@@ -5,6 +5,7 @@ import com.vipro.data.Address;
 public interface AddressService {
 
 	public Address findById(Long addressId);
+	public Address findByCustomerId(Long customerId);
 	public void insert(Address a);
 	public void update(Address a);
 	public void delete(Long addrId);
