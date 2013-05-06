2ecb0af641d0aa8b25e32da11b5c497ea7292d7c
lancelot.mok@gmail.com <lancelot.mok@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-05-06 04:08:22 +0000

    Check in by Lance: Sales module - Property Status amendment

--- trunk/vipro/src/com/vipro/constant/PropertyUnitStatusConst.java
+++ trunk/vipro/src/com/vipro/constant/PropertyUnitStatusConst.java
@@ -2,4 +2,6 @@ package com.vipro.constant;
 
 public interface PropertyUnitStatusConst extends CommonConst {
 	public static final String STATUS_SOLD="SOLD";
+	public static final String STATUS_IN_PROGRESS="PROG";
+	public static final String STATUS_BOOKED="BOOK";
 }
