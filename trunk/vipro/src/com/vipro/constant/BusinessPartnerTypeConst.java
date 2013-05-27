1ccad6f0b1cefd077bb004e318c867e57a14a6ec
lancelot.mok@gmail.com <lancelot.mok@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-05-27 05:58:41 +0000

    Check in by Lance: Sales Module - Business Partner for Developer and Land Proprietor

---
+++ trunk/vipro/src/com/vipro/constant/BusinessPartnerTypeConst.java
@@ -0,0 +1,9 @@
+package com.vipro.constant;
+
+public interface BusinessPartnerTypeConst extends CommonConst {
+
+	public static final String DEVELOPER = "DEVP";
+	public static final String LAND_PROPRIETOR = "LAND";
+	public static final String SOLICITOR = "SOLI";
+	public static final String BANK = "BANK";
+}
