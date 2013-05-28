7313b0c9d0beb264de9c0d0c575f7f7d34d150d1
lancelot.mok@gmail.com <lancelot.mok@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-05-28 02:43:33 +0000

    Check in by Lance: Sales Module - Jasper report modification

--- trunk/vipro/src/com/vipro/jsf/bean/mis/ReportBean.java
+++ trunk/vipro/src/com/vipro/jsf/bean/mis/ReportBean.java
@@ -1,12 +1,18 @@
 package com.vipro.jsf.bean.mis;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
+import javax.servlet.ServletContext;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import com.vipro.constant.JasperConst;
 import com.vipro.utils.spring.CodeUtil;
@@ -19,22 +25,12 @@ public class ReportBean {
 	private Long projectId;
 	
 	private List<SelectItem> listProject = null;
+	
+	private StreamedContent MISReport;
 
 	@PostConstruct
 	public void init() {
 		listProject = CodeUtil.getProjectAsItems();
-	}
-	
-	
-	public String genProjectPricingRpt() {
-		// generate project pricing report
-		HashMap<String, Object> hm = new HashMap<String, Object>();
-		hm.put("project_id", Long.toString(projectId));
-		
-		String report = JasperConst.MIS_PROJECT_PRICING;
-		JasperUtil.generateReport(hm, report);
-		
-		return "projectPricingRpt";
 	}
 
 	public Long getProjectId() {
@@ -53,4 +49,17 @@ public class ReportBean {
 		this.listProject = listProject;
 	}
 	
+	public StreamedContent getMISReport() {  
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		hm.put("project_id", Long.toString(projectId));
+		
+		String report = JasperConst.MIS_PROJECT_PRICING;
+		JasperUtil.generateReport(hm, report);
+		
+		String path ="/MISReport/MISReport.pdf";
+		InputStream stream = ((ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream(path);  
+        MISReport = new DefaultStreamedContent(stream, "MISReport.pdf", "MISReport.pdf"); 
+        
+        return MISReport;  
+    } 
 }
