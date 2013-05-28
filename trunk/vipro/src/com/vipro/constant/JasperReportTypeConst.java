1b11f0a94393440068a0c067c611df8b7e5adeac
lancelot.mok@gmail.com <lancelot.mok@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-05-28 02:17:30 +0000

    Check in by Lance: Sales Module - Jasper report modification

---
+++ trunk/vipro/src/com/vipro/constant/JasperReportTypeConst.java
@@ -0,0 +1,11 @@
+package com.vipro.constant;
+
+
+public class JasperReportTypeConst {
+
+	
+	public static final String REGISTRATION_FILE = "RegistrationForm.pdf";
+	
+	public static final String RECEIPT_FILE = "Receipt.pdf";
+
+}
