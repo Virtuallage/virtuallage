b2cb12edede6afb352c462f42fef363511b4b09f
tanhockweng@gmail.com <tanhockweng@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-07-07 04:23:25 +0000

    - Added constant files

---
+++ trunk/vipro/src/com/vipro/constant/PurchaseTypeConst.java
@@ -0,0 +1,16 @@
+/**
+ * Vipro - Virtuallage Property Development System 
+ * Copyright(C) 2013 Virtuallage Team  http://www.virtuallage.com 
+ * ---------------------------------------------------------------
+ */
+package com.vipro.constant;
+
+public interface PurchaseTypeConst extends CommonConst {
+	
+	public static final String BANK_LOAD="PUBLN";
+	
+	public static final String COMPANY_LOAD="PUCLN";
+	
+	public static final String CASH="PUCSH";
+
+}
---
+++ trunk/vipro/src/com/vipro/constant/UserGroupConst.java
@@ -0,0 +1,16 @@
+/**
+ * Vipro - Virtuallage Property Development System 
+ * Copyright(C) 2013 Virtuallage Team  http://www.virtuallage.com 
+ * ---------------------------------------------------------------
+ */
+package com.vipro.constant;
+
+public interface UserGroupConst extends CommonConst {
+	
+	public static final String SALES_PIC="SALES_PIC";
+	
+	public static final String ADMIN="ADMIN";
+	
+	public static final String SALES="SALES";
+
+}
