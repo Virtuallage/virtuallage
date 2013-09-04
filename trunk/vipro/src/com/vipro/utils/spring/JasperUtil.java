71952685d9a5e3c2b3dfcdc49e60df0c61a5c642
whdloi@gmail.com <whdloi@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2013-09-04 00:06:18 +0000

    1. Change Jasper Report Storage to accounts folder, remove reports folder. 2. New table creation sql for price adjustment

--- trunk/vipro/src/com/vipro/utils/spring/JasperUtil.java
+++ trunk/vipro/src/com/vipro/utils/spring/JasperUtil.java
@@ -44,7 +44,7 @@ public class JasperUtil {
 			String report, Account account, String reportType) {
 		String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
 		
-	    path = path.concat("reports/");
+	    path = path.concat("accounts/");
 	    File reportDirectory = new File(path);
 	    if (!reportDirectory.exists())
 	    {
