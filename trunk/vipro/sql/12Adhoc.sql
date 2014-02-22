33e972454b0f156fe1463e6229ea609e59def88a
whdloi@gmail.com <whdloi@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2014-02-22 12:26:51 +0000

    Version 1.2.1 Change length to string 50

--- trunk/vipro/sql/12Adhoc.sql
+++ trunk/vipro/sql/12Adhoc.sql
@@ -1,8 +1,8 @@
 ALTER TABLE vipro.project_inventory 
  ADD COLUMN net_price DEC(11,2) NOT NULL AFTER discount_amount;
  
-ALTER TABLE vipro.account change spa_ref_no spa_ref_no VARCHAR(40);
+ALTER TABLE vipro.account change spa_ref_no spa_ref_no VARCHAR(50);
 
-ALTER TABLE vipro.account change financier_ref financier_ref VARCHAR(40);
+ALTER TABLE vipro.account change financier_ref financier_ref VARCHAR(50);
 
-ALTER TABLE vipro.account change la_ref_no la_ref_no VARCHAR(40);
+ALTER TABLE vipro.account change la_ref_no la_ref_no VARCHAR(50);
