31236773e5a5092de69ebb4ebcf543a26331e9ac
whdloi@gmail.com <whdloi@gmail.com@51301ea1-c0b2-3802-d897-9577b65455b3>
2014-11-06 03:53:06 +0000

    Version 1.2.19 - migrate new billing listing by Invoice No.

--- vipro/src/com/vipro/jsf/bean/mis/ReportBean.java
+++ vipro/src/com/vipro/jsf/bean/mis/ReportBean.java
@@ -1,9 +1,11 @@
 package com.vipro.jsf.bean.mis;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -15,8 +17,12 @@ import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
 import com.vipro.constant.JasperConst;
+import com.vipro.data.Project;
+import com.vipro.service.ProjectService;
 import com.vipro.utils.spring.CodeUtil;
+import com.vipro.utils.spring.JasperCsvUtil;
 import com.vipro.utils.spring.JasperUtil;
+import com.vipro.utils.spring.SpringBeanUtil;
 
 @ManagedBean(name = "reportBean")
 @RequestScoped
@@ -27,6 +33,7 @@ public class ReportBean {
 	private List<SelectItem> listProject = null;
 	
 	private StreamedContent MISReport;
+	private StreamedContent ProgressiveBillingReport;
 
 	@PostConstruct
 	public void init() {
@@ -61,5 +68,38 @@ public class ReportBean {
         MISReport = new DefaultStreamedContent(stream, "MISReport.pdf", "MISReport.pdf"); 
         
         return MISReport;  
+    } 
+	
+	public StreamedContent getProgressiveBillingReport() {  
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		hm.put("projectId", Long.toString(projectId));
+		
+		Project project_name = new Project();
+		project_name.getProjectName();
+		ProjectService projectService = (ProjectService) SpringBeanUtil
+				.lookup(ProjectService.class.getName());
+		project_name = projectService.findById(projectId);
+		String outputfilename = "ProgressiveBillingListingByInvoiceNo" + project_name.getProjectCode() + ".pdf";
+		
+		MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+		String mimeType = mimeTypesMap.getContentType(outputfilename);
+				
+		String pathx = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
+	    pathx = pathx.concat("MISReport/");
+	    File reportDirectory = new File(pathx);
+	    if (!reportDirectory.exists())
+	    {
+	    	reportDirectory.mkdir();
+	    }
+
+			String report = JasperConst.PROGRESSIVE_BILLING_REPORT_BY_INVOICE_NO;
+			String pdf = pathx + "ProgressiveBillingListingByInvoiceNo.pdf";
+			JasperUtil.generateReport(hm, report, pdf);
+			String path ="/MISReport/ProgressiveBillingListingByInvoiceNo.pdf";
+			InputStream stream = ((ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream(path);
+			ProgressiveBillingReport = new DefaultStreamedContent(stream, mimeType, outputfilename);
+		
+
+        return ProgressiveBillingReport;  
     } 
 }
